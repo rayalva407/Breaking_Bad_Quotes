e6e2127126cddb3bcd80ce021836c2d512d2f342
rayalva407 <rayalva407@gmail.com>
2020-04-16 08:28:26 -0600

    adds cli menu

--- lib/Generator/cli.rb
+++ lib/Generator/cli.rb
@@ -1,5 +1,17 @@
 class Cli
     def run
 
+        puts " "
+        puts "Welcome to Breaking Quotes"
+        puts " "
+        puts "Enter 'list' to see all characters, 'random' to get a random quote, or 'exit' to quit"
 
+        input = gets.strip.downcase
+        if input = "list"
+            character_list
+        elsif input = "random"
+            Api.random_quote
+            
+        end
+    end
 end
