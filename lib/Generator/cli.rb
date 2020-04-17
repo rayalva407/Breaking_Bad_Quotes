afbf0bb2e3f30f8c4a0b36aaf316580d203bf274
rayalva407 <rayalva407@gmail.com>
2020-04-17 14:06:46 -0600

    makes code easier to read

--- lib/Generator/cli.rb
+++ lib/Generator/cli.rb
@@ -18,28 +18,31 @@ class Cli
             
             when "list"
                 print_characters
-
                 puts " "
                 puts "Enter a characters number to get some of their famous quotes."
                 puts " "
-
                 input = gets.strip.to_i
 
                 if input > 0 && input <= 63
-                    Api.character_quotes(Character.all[input.to_i - 1]) if Character.all[input.to_i - 1].quotes.length == 0
+                    Api.character_quotes(Character.all[input - 1]) if Character.all[input - 1].quotes.length == 0
+
                     if Character.all[input - 1].quotes.length > 0
-                        Character.all[input.to_i - 1].quotes.each.with_index(1) do |quote, i|
+
+                        Character.all[input - 1].quotes.each.with_index(1) do |quote, i|
                             puts " "
-                            puts "#{i}. #{quote.txt} -- #{Character.all[input.to_i - 1].name}"
+                            puts "#{i}. #{quote.txt} -- #{Character.all[input - 1].name}"
                         end
+
                     else
                         puts "There are no quotes for #{Character.all[input - 1].name} please try again."
                     end
+
                     puts "---------------------------------------------------------------------------------------------------"
                     puts "Enter 'list' to see another character's quotes, 'random' to get a random quote, or 'exit' to quit."
+
                 elsif input < 0 || input > 63
                     " "
-                    puts "Select a valid number please"
+                    puts "Select a valid number please."
                     puts "---------------------------------------------------------------------------------------------------"
                     puts "Enter 'list' to see another character's quotes, 'random' to get a random quote, or 'exit' to quit."
                 end
@@ -51,16 +54,18 @@ class Cli
                 puts "---------------------------------------------------------------------------------------------------"
                 puts "Enter 'list' to see another character's quotes, 'random' to get a random quote, or 'exit' to quit."
             end
-
         end
+
         puts " "
         puts "Good-bye"
     end
 
     def print_characters
+
         Character.all.each.with_index(1) do |char, i|
             puts "#{i}. #{char.name}"
         end
+
     end
 
 end
